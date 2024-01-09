d7d737869c7cbb43234d792b9f2894fb940c8450
lalitakapadia <lalita.kapadia@gmail.com>
2024-01-09 21:45:04 +0000

    worked on controller routes

---
+++ utils/handlebarshelpers.js
@@ -0,0 +1,12 @@
+module.exports = {
+    format_time: (date) => {
+      return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+      return date.toLocaleDateString();
+    },
+    format_datetime: (date) => {
+        return date.toLocaleTimeString()+" on "+ date.toLocaleDateString();
+      }
+  };
+  
